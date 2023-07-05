53c5fc69f7cef55513b24ac07edbbba59286389b
fabriUn2020 <fabricio.fa61@gmail.com>
2023-07-04 22:36:39 -0300

    Subo Clase ListaManager Tarea Codificar ListaManager

--- app/src/main/java/com/conejiaraapp/listapendientes/Clases/ListaManager.java
+++ app/src/main/java/com/conejiaraapp/listapendientes/Clases/ListaManager.java
@@ -1,6 +1,36 @@
 package com.conejiaraapp.listapendientes.Clases;
 
+import java.util.List;
+
 public class ListaManager
 {
+    public static List<String> listaDeTareas;
 
+    public void agregarLista(String nombreLista)
+    {
+        listaDeTareas.add(nombreLista);
+    }
+
+    public void editarLista(int indice, String nuevoNombre)
+    {
+        if (indice >= 0 && indice < listaDeTareas.size())
+        {
+            listaDeTareas.set(indice, nuevoNombre);
+        }
+        else
+        {
+            System.out.println("Índice inválido");
+        }
+    }
+    public void eliminarLista(int indice)
+    {
+        if (indice >= 0 && indice < listaDeTareas.size())
+        {
+            listaDeTareas.remove(indice);
+        }
+        else
+        {
+            System.out.println("Índice inválido");
+        }
+    }
 }
