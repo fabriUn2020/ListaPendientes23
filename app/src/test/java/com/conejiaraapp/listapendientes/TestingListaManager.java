0286bbe1532f9695b12518add30df1c5e8cf8689
fabriUn2020 <fabricio.fa61@gmail.com>
2023-07-06 17:44:29 -0300

    Subo Clase  TestingListaManager

---
+++ app/src/test/java/com/conejiaraapp/listapendientes/TestingListaManager.java
@@ -0,0 +1,13 @@
+package com.conejiaraapp.listapendientes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestingListaManager
+{
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
